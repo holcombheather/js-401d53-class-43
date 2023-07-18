addd3088ae99c751c3010d96848e2a21f0e47ea2
Heather Holcomb <holcombheather@gmail.com>
2023-07-18 11:43:35 -0700

    proof of life for camera and components for header and footer

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import { Camera, CameraType } from 'expo-camera';
 
 export default function Home() {
   const [date, setDate] = useState(new Date(1598051730000));
@@ -27,31 +28,78 @@ export default function Home() {
     showMode('time');
   };
 
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
 
   return (
-    <View style={styles.container}>
-             <Text>Hello lab 43!</Text>
-    <Button onPress={showDatepicker} title="Show date picker!" />
-    <Button onPress={showTimepicker} title="Show time picker!" />
-    <Text>selected: {date.toLocaleString()}</Text>
-    {show && (
-      <DateTimePicker
-        testID="dateTimePicker"
-        value={date}
-        mode={mode}
-        // is24Hour={true}
-        // onChange={onChange}
-      />
-    )}
+    <><View style={styles.container}>
+      <Text>Hello from home!</Text>
+      <Button onPress={showDatepicker} title="Show date picker!" />
+      <Button onPress={showTimepicker} title="Show time picker!" />
+      <Text>selected: {date.toLocaleString()}</Text>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode} />
+      )}
     </View>
+
+    {/* <View style={styles.container}> */}
+    <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      {/* </View> */}
+      </>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
